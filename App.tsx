8b36418203635f9e6bf23f0482766a157a8a2ca1
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2023-07-24 10:08:50 +0900

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { View } from 'react-native-rom-components'
+import { LogBox, Text } from 'react-native'
+LogBox.ignoreAllLogs()
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
+    <View flex1 main='center' cross='center'>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
